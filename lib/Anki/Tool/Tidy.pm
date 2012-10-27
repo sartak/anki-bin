4ea062ea1de2a208946747b93809cc860831c08f
Shawn M Moore <code@sartak.org>
2012-10-27 13:22:04 -0400

    Anki tidy

---
+++ lib/Anki/Tool/Tidy.pm
@@ -0,0 +1,22 @@
+package Anki::Tool::Tidy;
+use utf8;
+use 5.16.0;
+use warnings;
+use Any::Moose;
+
+extends 'Anki::Tool';
+
+sub each_field {
+    my ($self, $field) = @_;
+    my $value = $field->value;
+
+    if ($value =~ s/^(\s+)|(\s+)$/\e[1;41m$+\e[m/g) {
+        warn $field->note_id . '|' . $value . "\n";
+        return 0;
+    }
+
+    return 1;
+}
+
+1;
+
