16802db21df2fdb9bc1e40363bf68d3e71f9c80e
Shawn M Moore <code@sartak.org>
2012-10-27 13:21:11 -0400

    Start Anki::Tool

---
+++ lib/Anki/Tool.pm
@@ -0,0 +1,26 @@
+package Anki::Tool;
+use utf8;
+use 5.16.0;
+use warnings;
+use Any::Moose;
+
+use Anki::Database;
+
+has dbh => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { Anki::Database->new },
+);
+
+sub inspect {
+    my $self = shift;
+    my $dbh = $self->dbh;
+
+    if ($self->can('each_field')) {
+        $dbh->each_field(sub { $self->each_field(@_) });
+    }
+}
+
+1;
+
+
