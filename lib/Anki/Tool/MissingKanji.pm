c913f4387a3e8c48a50874944124e01c7eb202e1
Shawn M Moore <code@sartak.org>
2012-12-27 17:23:36 -0500

    Port missing-kanji

---
+++ lib/Anki/Tool/MissingKanji.pm
@@ -0,0 +1,43 @@
+package Anki::Tool::MissingKanji;
+use utf8;
+use 5.16.0;
+use warnings;
+use Any::Moose;
+
+extends 'Anki::Tool';
+
+my %sentence_kanji;
+my %studied_kanji;
+
+sub each_note_文 {
+    my ($self, $note) = @_;
+    my $sentence = $note->field('日本語');
+
+    $sentence_kanji{$_} = $note
+        for $sentence =~ /\p{Han}/g;
+
+    return 1;
+}
+
+sub each_note_漢字 {
+    my ($self, $note) = @_;
+    my $kanji = $note->field('漢字');
+    $studied_kanji{$kanji}++;
+    delete $sentence_kanji{$_};
+}
+
+sub done {
+    my ($self) = @_;
+
+    delete $sentence_kanji{'々'};
+    delete $sentence_kanji{'〇'};
+
+    for my $kanji (keys %sentence_kanji) {
+        next if $studied_kanji{$kanji};
+        my $note = $sentence_kanji{$kanji};
+        $self->report_note($note, $note->field('日本語') . " - includes missing kanji $kanji");
+    }
+}
+
+1;
+
