0d88bf5547e0a2d779c9a78583e14a277f2e5c38
Shawn M Moore <shawn@bestpractical.com>
2016-06-10 23:40:25 -0400

    SGF validator

---
+++ lib/Anki/Tool/SGF.pm
@@ -0,0 +1,26 @@
+package Anki::Tool::SGF;
+use utf8;
+use 5.16.0;
+use warnings;
+use Any::Moose;
+use Games::Go::SGF::Grove 'decode_sgf';
+
+extends 'Anki::Tool';
+
+sub each_note_詰碁 {
+    my ($self, $note) = @_;
+
+    my $sgf = $note->field('SGF');
+    if ($sgf !~ m{\bCH\[1\]}) {
+        $self->report_note($note, "Missing CH[1] correct answer indicator");
+    }
+
+    # validate
+    eval { decode_sgf($sgf) };
+    if ($@) {
+        $self->report_note($note, $@);
+    }
+}
+
+1;
+
