e2ba7c5772c7654b37ba6d7f0ce09f949b3978c0
Shawn M Moore <code@sartak.org>
2020-09-26 17:30:50 +0000

    Report duplicate canto sentences

--- lib/Anki/Tool/DuplicateSentences.pm
+++ lib/Anki/Tool/DuplicateSentences.pm
@@ -6,17 +6,31 @@ use Any::Moose;
 
 extends 'Anki::Tool';
 
-my %seen;
+my %seen_j;
+my %seen_c;
 
 sub each_note_文 {
     my ($self, $note) = @_;
     my $sentence = $note->field('日本語');
 
-    if ($seen{ $sentence }) {
-        return $self->report_note($note, "$sentence - duplicate sentence with nid:$seen{$sentence}");
+    if ($seen_j{ $sentence }) {
+        return $self->report_note($note, "$sentence - duplicate sentence with nid:$seen_j{$sentence}");
     }
 
-    $seen{$sentence} = $note->id;
+    $seen_j{$sentence} = $note->id;
+
+    return 1;
+}
+
+sub each_note_廣東話文 {
+    my ($self, $note) = @_;
+    my $sentence = $note->field('廣東話');
+
+    if ($seen_c{ $sentence }) {
+        return $self->report_note($note, "$sentence - duplicate sentence with nid:$seen_c{$sentence}");
+    }
+
+    $seen_c{$sentence} = $note->id;
 
     return 1;
 }
